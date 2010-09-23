38367d8918263571bb688f8764adb7453ddaeb17
Administrator <Administrator@192.168.0.100>
2010-09-23 22:06:54 +0200

    working state

---
+++ src/jgpfun/world2d/Body2d.java
@@ -0,0 +1,33 @@
+package jgpfun.world2d;
+
+import jgpfun.Food;
+
+public class Body2d {
+
+    public final WallSense wallSense;
+
+    public final Motor2d motor;
+
+    public final FoodFinder foodFinder;
+
+    public Food food;
+
+    public double dir;
+
+    public int x;
+
+    public int y;
+
+
+    public Body2d(int x, int y, double dir, FoodFinder foodFinder, WallSense wallSense) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+
+        this.motor = new TankMotor(this);
+        this.foodFinder = foodFinder;
+        this.wallSense = wallSense;
+        wallSense.setBody(this);
+    }
+
+}
